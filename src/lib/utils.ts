aa6d23010e2196340b0f88bd91c9504a7a906376
NguyenQuocKhai0505 <nguyenquockhai05.contact@gmail.com>
2025-11-12 13:35:29 +0900

    validate slug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function slugify(text:string):string{
+      return text
+            .toLowerCase()
+            .trim()
+            .replace(/[^\w\s-]/g, '')        // Xóa ký tự đặc biệt
+            .replace(/[\s_-]+/g, '-')        // Thay space và _ bằng -
+            .replace(/^-+|-+$/g, '')         // Xóa - ở đầu/cuối 
+}
